101a1667362b209938150d6b822fc7e2500e2bd3
Daniel Riverol <dannyriverol@hotmail.com>
2025-12-06 12:33:24 -0300

    feat: add UUID dependency and implement order creation with total price calculation

--- clase_7/src/controllers/orders.controller.js
+++ clase_7/src/controllers/orders.controller.js
@@ -28,8 +28,28 @@ export const createOrder = async (req, res) => {
   try {
     const userFound = await userService.getUserById(user);
     const businessFound = await businessService.getBusinessById(business);
-    //continuará....
+    //traemos los productos que quiero agregar a la orden
+    const actualOrders= businessFound.products.filter((prod)=> products.includes(prod.id));
     
+
+    //calcualr el total
+    const total = actualOrders.reduce((acc, prod) => acc + prod.price, 0);
+
+    const newOrder= {
+      id: uuid(),
+      business, 
+      user,
+      status:"pending",
+      products: actualOrders.map((prod)=> prod.id), 
+      totalPrice:total
+    }
+
+    //Guardamos la orden
+    const createdOrder = await orderService.createOrder(newOrder);
+//Actualizamos el usuario y el negocio con la nueva orden
+userFound.orders.push(createdOrder._id);
+await userService.updateUser(userFound._id, userFound);
+    res.status(201).json({ status: "success", payload: createdOrder });
   } catch (error) {
     return res.status(500).json({ status: "Error", error: error.message });
   }
